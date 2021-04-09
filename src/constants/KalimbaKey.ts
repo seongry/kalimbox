108d94f8f2c1ee61568e2b81f61c4bab29eaaea6
SeongRyeong Kim <vev4747@gmail.com>
2021-04-10 00:30:00 +0900

    feat: Add constants data

---
+++ src/constants/KalimbaKey.ts
@@ -0,0 +1,93 @@
+export type KalimbaKeyBarsTypes = {
+    number: string;
+    code: string;
+    higher: number;
+  };
+  export const C_KEY: KalimbaKeyBarsTypes[] = [
+    {
+      number: '2',
+      code: 'D',
+      higher: 2,
+    },
+    {
+      number: '7',
+      code: 'B',
+      higher: 1,
+    },
+    {
+      number: '5',
+      code: 'G',
+      higher: 1,
+    },
+    {
+      number: '3',
+      code: 'E',
+      higher: 2,
+    },
+    {
+      number: '1',
+      code: 'C',
+      higher: 1,
+    },
+    {
+      number: '6',
+      code: 'A',
+      higher: 0,
+    },
+    {
+      number: '4',
+      code: 'F',
+      higher: 0,
+    },
+    {
+      number: '2',
+      code: 'D',
+      higher: 0,
+    },
+    {
+      number: '1',
+      code: 'C',
+      higher: 0,
+    },
+    {
+      number: '3',
+      code: 'E',
+      higher: 0,
+    },
+    {
+      number: '5',
+      code: 'G',
+      higher: 0,
+    },
+    {
+      number: '7',
+      code: 'B',
+      higher: 0,
+    },
+    {
+      number: '2',
+      code: 'D',
+      higher: 1,
+    },
+    {
+      number: '4',
+      code: 'F',
+      higher: 1,
+    },
+    {
+      number: '6',
+      code: 'A',
+      higher: 1,
+    },
+    {
+      number: '1',
+      code: 'C',
+      higher: 2,
+    },
+    {
+      number: '3',
+      code: 'E',
+      higher: 2,
+    },
+  ];
+  
