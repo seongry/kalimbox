776a2b0b06b3bc3e2e45867a8ea029db06a7cc16
SeongRyeong Kim <vev4747@gmail.com>
2021-04-10 12:53:51 +0900

    chore: Apply prettier

--- rollup.config.js
+++ rollup.config.js
@@ -1,98 +1,105 @@
-import commonjs from '@rollup/plugin-commonjs';
-import resolve from '@rollup/plugin-node-resolve';
-import typescript from '@rollup/plugin-typescript';
-import css from 'rollup-plugin-css-only';
-import livereload from 'rollup-plugin-livereload';
-import svelte from 'rollup-plugin-svelte';
-import { terser } from 'rollup-plugin-terser';
-import sveltePreprocess from 'svelte-preprocess';
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import typescript from "@rollup/plugin-typescript";
+import css from "rollup-plugin-css-only";
+import livereload from "rollup-plugin-livereload";
+import svelte from "rollup-plugin-svelte";
+import { terser } from "rollup-plugin-terser";
+import sveltePreprocess from "svelte-preprocess";
 
 const production = !process.env.ROLLUP_WATCH;
 
 function serve() {
-	let server;
+  let server;
 
-	function toExit() {
-		if (server) server.kill(0);
-	}
+  function toExit() {
+    if (server) server.kill(0);
+  }
 
-	return {
-		writeBundle() {
-			if (server) return;
-			server = require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
-				stdio: ['ignore', 'inherit', 'inherit'],
-				shell: true
-			});
+  return {
+    writeBundle() {
+      if (server) return;
+      server = require("child_process").spawn(
+        "npm",
+        ["run", "start", "--", "--dev"],
+        {
+          stdio: ["ignore", "inherit", "inherit"],
+          shell: true,
+        }
+      );
 
-			process.on('SIGTERM', toExit);
-			process.on('exit', toExit);
-		}
-	};
+      process.on("SIGTERM", toExit);
+      process.on("exit", toExit);
+    },
+  };
 }
 
 export default {
-	input: 'src/main.ts',
-	output: {
-		sourcemap: true,
-		format: 'iife',
-		name: 'app',
-		file: 'public/build/bundle.js'
-	},
-	plugins: [
-		svelte({
-			preprocess: sveltePreprocess({ sourceMap: !production, 
-				babel: {
-				presets: [
-				  [
-					"@babel/preset-env",
-					{
-					  loose: true,
-					  modules: false,
-					  targets: {
-						esmodules: true,
-					  },
-					},
-				  ],
-				],
-			  },
-			  plugins: ["@babel/plugin-proposal-optional-chaining"],}),
-			compilerOptions: {
-				// enable run-time checks when not in production
-				dev: !production
-			}
-		}),
-		// we'll extract any component CSS out into
-		// a separate file - better for performance
-		css({ output: 'bundle.css' }),
+  input: "src/main.ts",
+  output: {
+    sourcemap: true,
+    format: "iife",
+    name: "app",
+    file: "public/build/bundle.js",
+  },
+  plugins: [
+    svelte({
+      preprocess: sveltePreprocess({
+        sourceMap: !production,
 
-		// If you have external dependencies installed from
-		// npm, you'll most likely need these plugins. In
-		// some cases you'll need additional configuration -
-		// consult the documentation for details:
-		// https://github.com/rollup/plugins/tree/master/packages/commonjs
-		resolve({
-			browser: true,
-			dedupe: ['svelte']
-		}),
-		commonjs(),
-		typescript({
-			sourceMap: !production,
-			inlineSources: !production
-		}),
+        babel: {
+          presets: [
+            [
+              "@babel/preset-env",
+              {
+                loose: true,
+                modules: false,
+                targets: {
+                  esmodules: true,
+                },
+              },
+            ],
+          ],
+        },
+        plugins: ["@babel/plugin-proposal-optional-chaining"],
+      }),
+      compilerOptions: {
+        // enable run-time checks when not in production
+        dev: !production,
+      },
+    }),
+    // we'll extract any component CSS out into
+    // a separate file - better for performance
+    css({ output: "bundle.css" }),
 
-		// In dev mode, call `npm run start` once
-		// the bundle has been generated
-		!production && serve(),
+    // If you have external dependencies installed from
+    // npm, you'll most likely need these plugins. In
+    // some cases you'll need additional configuration -
+    // consult the documentation for details:
+    // https://github.com/rollup/plugins/tree/master/packages/commonjs
+    resolve({
+      browser: true,
+      dedupe: ["svelte"],
+    }),
+    commonjs(),
+    typescript({
+      sourceMap: !production,
+      inlineSources: !production,
+    }),
 
-		// Watch the `public` directory and refresh the
-		// browser on changes when not in production
-		!production && livereload('public'),
+    // In dev mode, call `npm run start` once
+    // the bundle has been generated
+    !production && serve(),
 
-		// If we're building for production (npm run build
-		// instead of npm run dev), minify
-		production && terser()
-	],
-	watch: {
-		clearScreen: false
-	}
+    // Watch the `public` directory and refresh the
+    // browser on changes when not in production
+    !production && livereload("public"),
+
+    // If we're building for production (npm run build
+    // instead of npm run dev), minify
+    production && terser(),
+  ],
+  watch: {
+    clearScreen: false,
+  },
 };
--- src/components/Kalimba/Kalimba.svelte
+++ src/components/Kalimba/Kalimba.svelte
@@ -1,21 +1,19 @@
 <script lang="ts">
-    import { C_KEY } from "../../constants/KalimbaKey";
-    import Nut from './Nut.svelte';
+  import { C_KEY } from "../../constants/KalimbaKey";
+  import Nut from "./Nut.svelte";
 
-
-    let kalimbaKeys = C_KEY;
-
+  let kalimbaKeys = C_KEY;
 </script>
 
-<div class="kalimba">
-    {#each kalimbaKeys as keyBar}
-        <Nut key={keyBar}/>
-    {/each}
-</div>
-
 <style lang="scss">
-    .kalimba {
-        display: flex;
-        flex-direction: row;
-    }
+  .kalimba {
+    display: flex;
+    flex-direction: row;
+  }
 </style>
+
+<div class="kalimba">
+  {#each kalimbaKeys as keyBar}
+    <Nut key="{keyBar}" />
+  {/each}
+</div>
