ccc8b8ae15815aa4a70375ef32ec4f1dedc412d0
SeongRyeong Kim <vev4747@gmail.com>
2021-04-12 23:25:01 +0900

    feat: Add SaveButton comp

---
+++ src/components/SaveButton.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+</script>
+
+<style lang="scss">
+  @import "../theme/colors.scss";
+
+  div {
+    flex: 1;
+    margin: 20px 0;
+  }
+  .save-button {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 180px;
+    height: 48px;
+    background-color: $black;
+    border-radius: 8px;
+    cursor: pointer;
+    color: $white;
+    font-size: 18px;
+  }
+</style>
+
+<div>
+  <div class="save-button">save</div>
+</div>
