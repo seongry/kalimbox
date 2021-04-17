96cdebd133b67ae6112e9936578267b5dd992997
SeongRyeong Kim <vev4747@gmail.com>
2021-04-17 21:44:37 +0900

    chore: Update C key kalimba code value

--- src/constants/KalimbaKey.ts
+++ src/constants/KalimbaKey.ts
@@ -1,93 +1,92 @@
 export type KalimbaKeyBarsTypes = {
-    number: string;
-    code: string;
-    higher: number;
-  };
-  export const C_KEY: KalimbaKeyBarsTypes[] = [
-    {
-      number: '2',
-      code: 'D',
-      higher: 2,
-    },
-    {
-      number: '7',
-      code: 'B',
-      higher: 1,
-    },
-    {
-      number: '5',
-      code: 'G',
-      higher: 1,
-    },
-    {
-      number: '3',
-      code: 'E',
-      higher: 2,
-    },
-    {
-      number: '1',
-      code: 'C',
-      higher: 1,
-    },
-    {
-      number: '6',
-      code: 'A',
-      higher: 0,
-    },
-    {
-      number: '4',
-      code: 'F',
-      higher: 0,
-    },
-    {
-      number: '2',
-      code: 'D',
-      higher: 0,
-    },
-    {
-      number: '1',
-      code: 'C',
-      higher: 0,
-    },
-    {
-      number: '3',
-      code: 'E',
-      higher: 0,
-    },
-    {
-      number: '5',
-      code: 'G',
-      higher: 0,
-    },
-    {
-      number: '7',
-      code: 'B',
-      higher: 0,
-    },
-    {
-      number: '2',
-      code: 'D',
-      higher: 1,
-    },
-    {
-      number: '4',
-      code: 'F',
-      higher: 1,
-    },
-    {
-      number: '6',
-      code: 'A',
-      higher: 1,
-    },
-    {
-      number: '1',
-      code: 'C',
-      higher: 2,
-    },
-    {
-      number: '3',
-      code: 'E',
-      higher: 2,
-    },
-  ];
-  
+  number: string;
+  code: string;
+  higher: number;
+};
+export const C_KEY: KalimbaKeyBarsTypes[] = [
+  {
+    number: "2",
+    code: "D6",
+    higher: 2,
+  },
+  {
+    number: "7",
+    code: "B5",
+    higher: 1,
+  },
+  {
+    number: "5",
+    code: "G5",
+    higher: 1,
+  },
+  {
+    number: "3",
+    code: "E5",
+    higher: 2,
+  },
+  {
+    number: "1",
+    code: "C5",
+    higher: 1,
+  },
+  {
+    number: "6",
+    code: "A4",
+    higher: 0,
+  },
+  {
+    number: "4",
+    code: "F4",
+    higher: 0,
+  },
+  {
+    number: "2",
+    code: "D4",
+    higher: 0,
+  },
+  {
+    number: "1",
+    code: "C4",
+    higher: 0,
+  },
+  {
+    number: "3",
+    code: "E4",
+    higher: 0,
+  },
+  {
+    number: "5",
+    code: "G4",
+    higher: 0,
+  },
+  {
+    number: "7",
+    code: "B4",
+    higher: 0,
+  },
+  {
+    number: "2",
+    code: "D5",
+    higher: 1,
+  },
+  {
+    number: "4",
+    code: "F5",
+    higher: 1,
+  },
+  {
+    number: "6",
+    code: "A5",
+    higher: 1,
+  },
+  {
+    number: "1",
+    code: "C6",
+    higher: 2,
+  },
+  {
+    number: "3",
+    code: "E6",
+    higher: 2,
+  },
+];
