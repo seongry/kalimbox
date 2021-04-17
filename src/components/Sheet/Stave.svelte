78004b7045da3ef06414abe924c5fa673fcfa80c
SeongRyeong Kim <vev4747@gmail.com>
2021-04-17 22:04:52 +0900

    feat: Add Stave component

---
+++ src/components/Sheet/Stave.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  export let notes: string[];
+</script>
+
+<style lang="scss">
+  .stave {
+  }
+</style>
+
+<div class="stave">
+  {#each notes as note}
+    {note}
+  {/each}
+</div>
