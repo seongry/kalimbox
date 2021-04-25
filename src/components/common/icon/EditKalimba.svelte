605591ae6f7e4458559fe897cb62f0546ad32434
SeongRyeong Kim <vev4747@gmail.com>
2021-04-25 16:47:17 +0900

    feat: Add SVG Icon comps

---
+++ src/components/common/icon/EditKalimba.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  import editKalimba from "../../../asset/edit_kalimba_icon.svg";
+</script>
+
+{@html editKalimba}
---
+++ src/components/common/icon/Menu.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  import menu from "../../../asset/menu_icon.svg";
+</script>
+
+{@html menu}
