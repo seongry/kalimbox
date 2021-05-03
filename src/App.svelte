cad4c9995963c77e2f581750a45f2034d97f3c99
SeongRyeong Kim <vev4747@gmail.com>
2021-05-03 23:46:47 +0900

    chore: Remove router temporary

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,5 @@
 <script lang="ts">
-  import { Route, Router } from "svelte-routing";
   import Generator from "./page/Generator.svelte";
 </script>
 
-<Router url="/">
-  <Route path="/" component={Generator} />
-</Router>
+<Generator />
