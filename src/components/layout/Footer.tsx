824048b67938f30e309b3c27d9e0fbef4c7bd093
SeongRyeong Kim <vev4747@gmail.com>
2021-12-20 17:26:20 +0900

    chore: Add semicolumn role

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,11 +1,11 @@
 /** @jsx jsx */
 
-import { footer, introLink } from "@/components/layout/styles"
-import { jsx } from "@emotion/react"
-import { FC } from "react"
+import { footer, introLink } from "@/components/layout/styles";
+import { jsx } from "@emotion/react";
+import { FC } from "react";
 
 interface IntroLinkProps {
-    href: string
+    href: string;
 }
 export const IntroLink: FC<IntroLinkProps> = ({ children, ...props }) => (
     <a
@@ -16,10 +16,10 @@ export const IntroLink: FC<IntroLinkProps> = ({ children, ...props }) => (
     >
         {children}
     </a>
-)
+);
 export const Footer: FC = () => (
     <section css={[footer]}>
         Designed by <IntroLink href="#">@1000yjc</IntroLink> Developed by{" "}
         <IntroLink href="https://github.com/seongry">@seongry</IntroLink>
     </section>
-)
+);
