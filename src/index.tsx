42b3bd5e8ff88883b550eb612157c222b72dd96a
SeongRyeong Kim <vev4747@gmail.com>
2021-12-21 11:17:36 +0900

    chore: Add missed semicolumn

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
-import { MainView } from "@/views/Kalimba"
-import React from "react"
-import { render } from "react-dom"
-import "./theme/global.css"
+import { KalimbaView } from "@/views/Kalimba";
+import React from "react";
+import { render } from "react-dom";
+import "./theme/global.css";
 
-render(<MainView />, document.getElementById("app"))
+render(<KalimbaView />, document.getElementById("app"));
