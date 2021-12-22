8078f71f56d2f33faa430501b8e4e620f000ce99
SeongRyeong Kim <vev4747@gmail.com>
2021-12-22 13:32:27 +0900

    chore: Change type to interface

--- src/constants/KalimbaKey.ts
+++ src/constants/KalimbaKey.ts
@@ -1,105 +1,105 @@
-export type KalimbaKeyBarsTypes = {
+export interface KalimbaKeyBarsTypes {
     number: string;
     code: string;
     higher: number;
-  };
-  
-  export const SPACEBAR = "_";
-  export const C_KEY: KalimbaKeyBarsTypes[] = [
+}
+
+export const SPACEBAR = "_";
+export const C_KEY: KalimbaKeyBarsTypes[] = [
     {
-      number: "2",
-      code: "D6",
-      higher: 2,
+        number: "2",
+        code: "D6",
+        higher: 2,
     },
     {
-      number: "7",
-      code: "B5",
-      higher: 1,
+        number: "7",
+        code: "B5",
+        higher: 1,
     },
     {
-      number: "5",
-      code: "G5",
-      higher: 1,
+        number: "5",
+        code: "G5",
+        higher: 1,
     },
     {
-      number: "3",
-      code: "E5",
-      higher: 2,
+        number: "3",
+        code: "E5",
+        higher: 2,
     },
     {
-      number: "1",
-      code: "C5",
-      higher: 1,
+        number: "1",
+        code: "C5",
+        higher: 1,
     },
     {
-      number: "6",
-      code: "A4",
-      higher: 0,
+        number: "6",
+        code: "A4",
+        higher: 0,
     },
     {
-      number: "4",
-      code: "F4",
-      higher: 0,
+        number: "4",
+        code: "F4",
+        higher: 0,
     },
     {
-      number: "2",
-      code: "D4",
-      higher: 0,
+        number: "2",
+        code: "D4",
+        higher: 0,
     },
     {
-      number: "1",
-      code: "C4",
-      higher: 0,
+        number: "1",
+        code: "C4",
+        higher: 0,
     },
     {
-      number: "3",
-      code: "E4",
-      higher: 0,
+        number: "3",
+        code: "E4",
+        higher: 0,
     },
     {
-      number: "5",
-      code: "G4",
-      higher: 0,
+        number: "5",
+        code: "G4",
+        higher: 0,
     },
     {
-      number: "7",
-      code: "B4",
-      higher: 0,
+        number: "7",
+        code: "B4",
+        higher: 0,
     },
     {
-      number: "2",
-      code: "D5",
-      higher: 1,
+        number: "2",
+        code: "D5",
+        higher: 1,
     },
     {
-      number: "4",
-      code: "F5",
-      higher: 1,
+        number: "4",
+        code: "F5",
+        higher: 1,
     },
     {
-      number: "6",
-      code: "A5",
-      higher: 1,
+        number: "6",
+        code: "A5",
+        higher: 1,
     },
     {
-      number: "1",
-      code: "C6",
-      higher: 2,
+        number: "1",
+        code: "C6",
+        higher: 2,
     },
     {
-      number: "3",
-      code: "E6",
-      higher: 2,
+        number: "3",
+        code: "E6",
+        higher: 2,
     },
-  ];
-  
-  export type ScaleType = {
+];
+
+export type ScaleType = {
     type: "C_KEY";
     keys: KalimbaKeyBarsTypes[];
-  };
-  export const SCALE_TYPES: ScaleType[] = [
+};
+export const SCALE_TYPES: ScaleType[] = [
     {
-      type: "C_KEY",
-      keys: C_KEY,
+        type: "C_KEY",
+        keys: C_KEY,
     },
-  ];
+];
