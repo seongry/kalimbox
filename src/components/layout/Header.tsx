862afac1e655da59f78d0406868fca8848cf2f53
SeongRyeong Kim <vev4747@gmail.com>
2021-12-22 14:41:58 +0900

    feat: Give new cool name to my toy project

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,4 +4,4 @@ import { header } from "@/components/layout/styles";
 import { jsx } from "@emotion/react";
 import { FC } from "react";
 
-export const Header: FC = () => <nav css={[header]}>칼림바 악보생성기</nav>;
+export const Header: FC = () => <nav css={[header]}>칼림박스</nav>;
