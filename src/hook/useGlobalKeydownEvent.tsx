28ea3b7189c7865582ab3452a544086b5f3e9ba6
SeongRyeong Kim <vev4747@gmail.com>
2021-12-23 17:42:26 +0900

    feat: Add useGlobalKeydownEvent hook

---
+++ src/hook/useGlobalKeydownEvent.tsx
@@ -0,0 +1,36 @@
+import { sheetController } from "@/controllers/sheet";
+import { useEffect } from "react";
+
+enum SPECIAL_KEYBOARD_KEYS {
+    Space = "Space",
+    Backspace = "Backspace",
+}
+export const useGlobalKeydownEvent = () => {
+    const { addSpacebar, removeLastNote } = sheetController();
+    const handleKeydown = (e: globalThis.KeyboardEvent) => {
+        if (
+            !Object.values(SPECIAL_KEYBOARD_KEYS).includes(
+                e.code as SPECIAL_KEYBOARD_KEYS
+            )
+        ) {
+            return;
+        }
+        if (e.code === SPECIAL_KEYBOARD_KEYS["Space"]) {
+            addSpacebar();
+        }
+
+        if (e.code === SPECIAL_KEYBOARD_KEYS["Backspace"]) {
+            //removeLastNote();
+        }
+    };
+    useEffect(() => {
+        const isSupported = window.addEventListener;
+        if (!isSupported) return;
+
+        window.addEventListener("keydown", handleKeydown);
+
+        return () => {
+            window.removeEventListener("keydown", handleKeydown);
+        };
+    }, []);
+};
