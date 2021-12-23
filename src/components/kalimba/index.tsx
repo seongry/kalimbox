8081eb08c1ccdd930652852085553f2f00aecca9
SeongRyeong Kim <vev4747@gmail.com>
2021-12-23 17:43:06 +0900

    feat: Apply KeydownEvent hook on Presentation

--- src/components/kalimba/index.tsx
+++ src/components/kalimba/index.tsx
@@ -8,17 +8,22 @@ import {
     saveButton,
     sheetSection,
 } from "@/components/kalimba/styles";
+import { useGlobalKeydownEvent } from "@/hook/useGlobalKeydownEvent";
 import { jsx } from "@emotion/react";
 import { FC } from "react";
 
-export const Presentation: FC = () => (
-    <div css={contents}>
-        <div css={sheetSection}>
-            <Sheet />
+export const Presentation: FC = () => {
+    useGlobalKeydownEvent();
+
+    return (
+        <div css={contents}>
+            <div css={sheetSection}>
+                <Sheet />
+            </div>
+            <div css={kalimbaSection}>
+                <Kalimba />
+                <button css={saveButton}>save</button>
+            </div>
         </div>
-        <div css={kalimbaSection}>
-            <Kalimba />
-            <button css={saveButton}>save</button>
-        </div>
-    </div>
-);
+    );
+};
