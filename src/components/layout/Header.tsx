30608cb6befcea69d597c9fe244e8fb03ffc8e81
SeongRyeong Kim <vev4747@gmail.com>
2021-12-30 12:17:23 +0900

    feat: Add ListButton on Header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,16 +2,23 @@
 
 import listIcon from "@/asset/menu_icon.svg";
 import { floatBox, header, iconButton } from "@/components/layout/styles";
+import { MODAL, settingController } from "@/controllers/setting";
 import { jsx } from "@emotion/react";
 import { FC } from "react";
 
+const ListButton: FC = () => {
+    const { openModal } = settingController();
+    return (
+        <button css={iconButton} onClick={() => openModal(MODAL["SHEET_LIST"])}>
+            <img src={listIcon} alt="sheet_list_icon" />
+            <span>악보집</span>
+        </button>
+    );
+};
 export const Header: FC = () => (
     <nav css={header}>
         <div css={floatBox}>
-            <button css={iconButton}>
-                <img src={listIcon} alt="sheet_list_icon" />
-                <span>악보집</span>
-            </button>
+            <ListButton />
         </div>
         칼림박스
     </nav>
