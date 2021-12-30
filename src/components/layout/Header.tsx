8d4a9baf4c9c613f2daafde26d0c30947554afa0
SeongRyeong Kim <vev4747@gmail.com>
2021-12-30 15:50:59 +0900

    feat: Change to call loadSheetList on Header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,13 +3,19 @@
 import listIcon from "@/asset/menu_icon.svg";
 import { floatBox, header, iconButton } from "@/components/layout/styles";
 import { MODAL, settingController } from "@/controllers/setting";
+import { sheetListController } from "@/controllers/sheetList";
 import { jsx } from "@emotion/react";
 import { FC } from "react";
 
 const ListButton: FC = () => {
+    const { loadSheetList } = sheetListController();
     const { openModal } = settingController();
+    const handleOnClick = () => {
+        loadSheetList();
+        openModal(MODAL["SHEET_LIST"]);
+    };
     return (
-        <button css={iconButton} onClick={() => openModal(MODAL["SHEET_LIST"])}>
+        <button css={iconButton} onClick={handleOnClick}>
             <img src={listIcon} alt="sheet_list_icon" />
             <span>악보집</span>
         </button>
