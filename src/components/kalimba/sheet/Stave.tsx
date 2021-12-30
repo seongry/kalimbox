bca806bab0dfa101820ceed318f474c4ebfe6d55
SeongRyeong Kim <vev4747@gmail.com>
2021-12-30 16:05:45 +0900

    feat: Scroll to down when notes overflow visible area

--- src/components/kalimba/sheet/Stave.tsx
+++ src/components/kalimba/sheet/Stave.tsx
@@ -4,18 +4,24 @@ import { Note } from "@/components/kalimba/sheet/Note";
 import { stave } from "@/components/kalimba/sheet/styles";
 import { sheetController } from "@/controllers/sheet";
 import { jsx } from "@emotion/react";
-import { FC } from "react";
+import { FC, useEffect, useRef } from "react";
 import { useRecoilValue } from "recoil";
 
 export const Stave: FC = () => {
+    const bottomRef = useRef<HTMLSpanElement | null>(null);
     const { sheetState } = sheetController();
     const { notes } = useRecoilValue(sheetState);
+
+    useEffect(() => {
+        bottomRef.current?.scrollIntoView();
+    }, [notes]);
 
     return (
         <div css={stave}>
             {notes.map((note, key) => (
                 <Note key={key} note={note} />
             ))}
+            <span ref={bottomRef} />
         </div>
     );
 };
