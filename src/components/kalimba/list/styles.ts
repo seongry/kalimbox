63adf69ccde2dac496c3628bbd91cf6b5744e152
SeongRyeong Kim <vev4747@gmail.com>
2021-12-30 12:16:26 +0900

    style: Add modalBackground style

--- src/components/kalimba/list/styles.ts
+++ src/components/kalimba/list/styles.ts
@@ -5,11 +5,19 @@ import { css } from "@emotion/react";
 export const modalWrapper = css`
     width: 100%;
     height: 100%;
-    position: absolute;
+    position: fixed;
     top: 0;
     left: 0;
     z-index: 10;
-    background-color: rgba(0, 0, 0, 0.5);
+`;
+export const modalBackground = css`
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    top: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0.25);
+    z-index: -1;
 `;
 export const modalBox = css`
     display: flex;
